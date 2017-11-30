626664fbe06fac181cc9a5bc8c836322fea8e9ed
Lawrence Getubig <lawrencegetubig@lars-larsonson.home>
2017-11-30 12:25:06 -0500

    remove drop shadows, make cta buttons' styles look the same for easy recognition, refactor js quickPick fx, optimize styles of color picker for old Safari browsers, start clean up of html

--- designs4.js
+++ designs4.js
@@ -1,6 +1,9 @@
-// set up grid
-var gridActive =  false;// set up conditional boolean for grid
 
+var gridActive =  false;// set up conditional boolean for grid
+// alert instructions string
+var alertInstructions = "- Click a square to fill with colour\n- Double click to unfill\n- Choose a colour using the picker or quick swatch!";
+
+// set up grid
 function makeGrid(evt){
     // prevent default event on click
     evt.preventDefault();	
@@ -20,25 +23,26 @@ function makeGrid(evt){
 	    }
 
         else if (!gridActive) {
+        	alert(alertInstructions)  
 			$(".flexbox-grid-container").addClass("visible");
 			addGridUnit(gridH, gridW);
-			gridActive = true;	    
+			gridActive = true;	  
 		}       
     }
 
     else {
-        alert("Choose grid values between 1 and 20 only!");
+        alert("Tut-tut! Choose grid values between 1 and 25 only!");
     }
 
 	changeColor();
 	clearColor();
-	quickPick();
 }// end makeGrid
 
 
 /* *********************************** makeGrid helper functions ************************************* */
 function isValid(gridH, gridW) {
-	if ( (gridH > 0) && (gridH < 21) && (gridW > 0) && (gridW < 21) )  {
+	//want to limit and check a grid of 25x25 squares
+	if ( (gridH > 0) && (gridH < 26) && (gridW > 0) && (gridW < 26) )  {
 		return true; 
 	}
 
@@ -48,7 +52,7 @@ function isValid(gridH, gridW) {
 }// end isValid
 
 function addGridUnit(gridH, gridW) {
-	var flexboxW = gridW*25;// 25 because that's the px width of each square/grid unit
+	var flexboxW = gridW*20;// 20 because that's the px width of each square/grid unit
 	//set flexbox-grid-container width
     $(".flexbox-grid-container").css("width", flexboxW);
 
@@ -74,27 +78,28 @@ function clearColor() {
     });
 } //end changeColor
 
-function quickPick() {
-$(".quickPick-panel").on("click", "div.swatch", function(evt){
-	var target = $(this).attr('id');
-	if (target === "swatch_5C4B51") {
-	$("#colorPicker").val("#5C4B51");
-	}
-	else if (target === "swatch_8CBEB2") {
-	$("#colorPicker").val("#8CBEB2");
-	}
-	else if (target === "swatch_F2EBBF") {
-	$("#colorPicker").val("#F2EBBF");
-	}
-	else if (target === "swatch_F3B562") {
-	$("#colorPicker").val("#F3B562");
-	}
-	else {
-	$("#colorPicker").val("#F06060");
-	}		
-});
 
-}
+function quickPick() {
+	$(".quickPick-panel").on("click", "div.swatch", function(evt){
+		var targetId = $(this).attr('id');
+		if (targetId === "swatch_5C4B51") {
+			$("#colorPicker").val("#5C4B51");
+		}
+		else if (targetId === "swatch_8CBEB2") {
+			$("#colorPicker").val("#8CBEB2");
+		}
+		else if (targetId === "swatch_F2EBBF") {
+			$("#colorPicker").val("#F2EBBF");
+		}
+		else if (targetId === "swatch_F3B562") {
+			$("#colorPicker").val("#F3B562");
+		}
+		else {
+			$("#colorPicker").val("#F06060");
+		}		
+	});
+}// end quickPick
+
 /* *********************************** more functions ************************************* */
 function shiftPanel() {
 $(".console-panel").toggleClass("console-panel-right");
@@ -105,7 +110,8 @@ $(".console-panel").toggleClass("console-panel-right");
 // When size is submitted by the user, call makeGrid()
 $("#submitSize").on("click", makeGrid);
 
-
+// When a swatch color is clicked, delegate the swatch colour to the color picker
+$(".quickPick-panel").on("click", "div.swatch", quickPick);
 
 // When user wants the UI panel shifted left or right
 $("#shiftPanel").on("click", shiftPanel);
