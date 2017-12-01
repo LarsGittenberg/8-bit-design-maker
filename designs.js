6def9d32fb3f1427c9f72ea17325bfefce74bc70
Lawrence Getubig <lawrencegetubig@lars-larsonson.home>
2017-11-30 22:20:52 -0500

    change styles of h1 title, delete preliminary test files

--- designs.js
+++ designs.js
@@ -1,89 +1,119 @@
-// set up grid
+
 var gridActive =  false;// set up conditional boolean for grid
 
+// alert instructions string
+var alertInstructions = "- Click a square to fill with colour\n- Double click to unfill\n- Choose a colour using the picker or quick swatch!";
 
+// set up grid
 function makeGrid(evt){
-// prevent default event on click
-    evt.preventDefault();
-// get grid height and width value
+    // prevent default event on click
+    evt.preventDefault();	
+
+	// get grid height and width value
     var gridH = $("#input_height").val();
     var gridW = $("#input_width").val();
 
-    if ( gridH < 1 || gridH > 15 || gridW < 1 || gridW > 15) {
-        alert("Choose a grid value between 1 and 15")
+    //validate that input is between 1 and 20
+    if (isValid(gridH, gridW) === true) {
+     	if (gridActive) {
+	        var overwriteResponse = confirm("Overwrite current grid?");
+	        if (overwriteResponse) {
+	            $("div.grid-unit").remove();
+	            addGridUnit(gridH, gridW);
+	        }
+	    }
+
+        else if (!gridActive) {
+        	alert(alertInstructions)  
+			$(".flexbox-grid-container").addClass("visible");
+			addGridUnit(gridH, gridW);
+			gridActive = true;	  
+		}       
     }
 
-    else if (gridActive===false) {
-// make flexbox-grid-container visible by adding 'visible' class
-        $(".flexbox-grid-container").addClass("visible");
-// calculate and set flexbox-grid-container width
-        var flexboxW = gridW*30;// 30 because that's the px width of each square unit
-        $(".flexbox-grid-container").css("width", flexboxW);
-// calculate how many squares to populate
-        var squareCount = gridH*gridW;
-        for (var i = 0; i < squareCount; i++) {
-        $(".flexbox-grid-container").append("<div class='grid-unit'></div>");
-        }// end for loop
+    else {
+        alert("Tut-tut! Choose grid values between 1 and 25 only!");
+    }
 
-// changing the colors on the grid
-        $(".grid-unit").on( 'mousedown', function( evt ) {
-            var hexVal = $("#colorPicker").val();
-            $( evt.target ).css( 'background', hexVal);
-        });// 
-
-// set conditional boolean for grid to true
-        gridActive = true;
-    }// end else if
-
-    else if (gridActive===true) {
-    alert("You have a grid already!\nDelete grid to make another.")
-    }// end else if 
+	changeColor();
+	clearColor();
 }// end makeGrid
 
 
+/* *********************************** makeGrid helper functions ************************************* */
+function isValid(gridH, gridW) {
+	//want to limit and check for a grid of 25x25 squares
+	if ( (gridH > 0) && (gridH < 26) && (gridW > 0) && (gridW < 26) )  {
+		return true; 
+	}
 
-function deleteGrid(evt) {
-// prevent default event on click
-    evt.preventDefault();
+	else {
+		return false;
+	}
+}// end isValid
 
-// confirm pop up to make sure user wants to delete
-    if (gridActive === false) {
-        alert("No grid to delete, silly.")
-    }// end if
+function addGridUnit(gridH, gridW) {
+	var flexboxW = gridW*20;// 20 because that's the px width of each square/grid unit
+	//set flexbox-grid-container width
+    $(".flexbox-grid-container").css("width", flexboxW);
 
-    else if (gridActive === true) {    
-        var deleteResp = confirm("Delete?");
+    // calculate how many squares/grid-units to populate
+    var howManySquares = gridH*gridW;
+    for (var i = 0; i < howManySquares; i++) {
+        $(".flexbox-grid-container").append("<div class='grid-unit'></div>");
+    }// end for loop
+}// end addGridUnit
 
-        if (deleteResp === true) {
-// remove flexbox-grid container by taking away its class 'visible'
-             $(".flexbox-grid-container").removeClass("visible");
-// set conditional boolean for grid to false   
-            gridActive = false;
-            $(".grid-unit").each(function removeSquares() {
-                $(".grid-unit").remove();// remove square units clearing grid with removeSquares function
-                }// end removeSquares 
-            );
-        }// end if
-    }// end else if  
-}// end deleteGrid
+function changeColor() {
+	// changing the colors of a square/pixel/grid-unit with one click
+    $(".grid-unit").on( 'mousedown', function( evt ) {
+        var hexVal = $("#colorPicker").val();
+        $( evt.target ).css( 'background', hexVal);
+    });
+} //end changeColor
+
+function clearColor() {
+	// clear the color of a square/pixel/grid-unit with double-click
+    $(".grid-unit").on( 'dblclick', function( evt ) {
+        $( evt.target ).css( 'background', "none");
+    });
+} //end changeColor
 
 
+function quickPick() {
+	$(".quickPick-panel").on("click", "div.swatch", function(evt){
+		var targetId = $(this).attr('id');
+		if (targetId === "swatch_5C4B51") {
+			$("#colorPicker").val("#5C4B51");
+		}
+		else if (targetId === "swatch_8CBEB2") {
+			$("#colorPicker").val("#8CBEB2");
+		}
+		else if (targetId === "swatch_F2EBBF") {
+			$("#colorPicker").val("#F2EBBF");
+		}
+		else if (targetId === "swatch_F3B562") {
+			$("#colorPicker").val("#F3B562");
+		}
+		else {
+			$("#colorPicker").val("#F06060");
+		}		
+	});
+}// end quickPick
 
+/* *********************************** more functions ************************************* */
 function shiftPanel() {
-    $(".console-panel").toggleClass("console-panel-right");
-}
+$(".panel").toggleClass("all-panel-right");
+}// end shiftPanel
 
-/* ********* UI event listeners ************ */
 
+/* *********************************** UI event listeners *********************************** */
 // When size is submitted by the user, call makeGrid()
 $("#submitSize").on("click", makeGrid);
-//  When use wants to erase squares but keep grid
-$("#deleteGrid").on("click", deleteGrid);
-// When user want to reset colors on the grid, aka 'clean slate'
-$('#colorReset').on('click', function(evt) {
-    console.log("clicked on button")
- $(".grid-unit").css('background', '#383838');
-});
+
+// When a swatch color is clicked, delegate the swatch colour to the color picker
+$(".quickPick-panel").on("click", "div.swatch", quickPick);
+
 // When user wants the UI panel shifted left or right
 $("#shiftPanel").on("click", shiftPanel);
 
