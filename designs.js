e2ac8f26236be4061b842e2d5ae7960d4271a62b
Lawrence Getubig <lawrencegetubig@lars-larsonson.home>
2017-12-02 12:07:26 -0500

    set body to max width 1920px, edit js, css and html to udacity style guide specs, add media query for 1020px breakpoint, add viewport meta tag for better tablet device experience, add hover styles to quickPick swatches, remove trailing spaces to code, change bkg color of size picker area to muted gray/green for better visual UI/UX, change var keyword to let in js,

--- designs.js
+++ designs.js
@@ -2,39 +2,37 @@
 let gridActive =  false;// set up conditional boolean for grid
 
 // alert instructions string
-const alertInstructions = "- Click a square to fill with colour\n-" + 
-	"Double click to unfill\n-" + 
-	"Choose a colour using the picker or quick swatch!";
+const alertInstructions = '- Click a square to fill with colour\n' +
+	'- Double click to unfill\n' +
+	'- Choose a colour by clicking the picker or quick swatch!';
 
 // set up grid
 function makeGrid(evt){
     // prevent default event on click
-    evt.preventDefault();	
+    evt.preventDefault();
 
-	// get grid height and width value
-    let gridH = $("#input_height").val();
-    let gridW = $("#input_width").val();
+	// get grid height and width value, assign to variables
+    let gridH = $('#input_height').val();
+    let gridW = $('#input_width').val();
 
     //validate that input is between 1 and 20
     if (isValid(gridH, gridW) === true) {
      	if (gridActive) {
-	        const overwriteResponse = confirm("Overwrite current grid?");
+	        const overwriteResponse = confirm('Overwrite current grid? You should know you can\'t undo this.');
 	        if (overwriteResponse) {
-	            $("div.grid-unit").remove();
+	            $('div.grid-unit').remove();
 	            addGridUnit(gridH, gridW);
 	        }
-	    }
 
-        else if (!gridActive) {
-        	alert(alertInstructions)  
-			$(".flexbox-grid-container").addClass("visible");
+     } else if (!gridActive) {
+        	alert(alertInstructions)
+			$('.flexbox-grid-container').addClass('visible');
 			addGridUnit(gridH, gridW);
-			gridActive = true;	  
-		}       
-    }
+			gridActive = true;
+		}
 
-    else {
-        alert("Tut-tut! Choose grid values between 1 and 25 only!");
+    } else {
+        alert('Tut-tut! Choose grid values between 1 and 25 only!');
     }
 
 	changeColor();
@@ -46,10 +44,9 @@ function makeGrid(evt){
 function isValid(gridH, gridW) {
 	//want to limit and check for a grid of 25x25 squares
 	if ( (gridH > 0) && (gridH < 26) && (gridW > 0) && (gridW < 26) )  {
-		return true; 
-	}
+		return true;
 
-	else {
+	} else {
 		return false;
 	}
 }// end isValid
@@ -57,67 +54,69 @@ function isValid(gridH, gridW) {
 function addGridUnit(gridH, gridW) {
 	let flexboxW = gridW*20;// 20 because that's the px width of each square/grid unit
 	//set flexbox-grid-container width
-    $(".flexbox-grid-container").css("width", flexboxW);
+    $('.flexbox-grid-container').css('width', flexboxW);
 
     // calculate how many squares/grid-units to populate
     let howManySquares = gridH*gridW;
-    for (var i = 0; i < howManySquares; i++) {
-        $(".flexbox-grid-container").append("<div class='grid-unit'></div>");
+    for (var i = 0; i < howManySquares; i++) {//I can't use const i = 0; per Udacity style guide
+    	//doesn't work in my Chrome browser
+        $('.flexbox-grid-container').append('<div class="grid-unit"></div>');
     }// end for loop
 }// end addGridUnit
 
 function changeColor() {
 	// changing the colors of a square/pixel/grid-unit with one click
-    $(".grid-unit").on( 'mousedown', function( evt ) {
-        let hexVal = $("#colorPicker").val();
+    $('.grid-unit').on( 'mousedown', function( evt ) {
+        let hexVal = $('#colorPicker').val();
         $( evt.target ).css( 'background', hexVal);
     });
-} //end changeColor
+} // end changeColor
 
 function clearColor() {
 	// clear the color of a square/pixel/grid-unit with double-click
-    $(".grid-unit").on( 'dblclick', function( evt ) {
-        $( evt.target ).css( 'background', "none");
+    $('.grid-unit').on( 'dblclick', function( evt ) {
+        $( evt.target ).css( 'background', 'none');
     });
-} //end changeColor
+} // end changeColor
 
 
 function quickPick() {
-	$(".quickPick-panel").on("click", "div.swatch", function(evt){
-		let targetId = $(this).attr('id');
-		if (targetId === "swatch_5C4B51") {
-			$("#colorPicker").val("#5C4B51");
-		}
-		else if (targetId === "swatch_8CBEB2") {
-			$("#colorPicker").val("#8CBEB2");
-		}
-		else if (targetId === "swatch_F2EBBF") {
-			$("#colorPicker").val("#F2EBBF");
-		}
-		else if (targetId === "swatch_F3B562") {
-			$("#colorPicker").val("#F3B562");
-		}
-		else {
-			$("#colorPicker").val("#F06060");
-		}		
-	});
+	// get the id/color identifier of the div.swatch area that was clicked
+	let targetId = $(this).attr('id');
+	if (targetId === 'swatch_5c4b51') {
+		$('#colorPicker').val('#5c4b51');
+
+	} else if (targetId === 'swatch_8cbeb2') {
+		$('#colorPicker').val('#8cbeb2');
+
+	} else if (targetId === 'swatch_f2ebbf') {
+		$('#colorPicker').val('#f2ebbf');
+
+	} else if (targetId === 'swatch_f3b562') {
+		$('#colorPicker').val('#f3b562');
+
+	} else {
+		$('#colorPicker').val('#f06060');
+	}
 }// end quickPick
 
 /* *********************************** more functions ************************************* */
 function shiftPanel() {
-$(".panel").toggleClass("all-panel-right");
+$('.panel').toggleClass('all-panel-right');
 }// end shiftPanel
 
 
 /* *********************************** UI event listeners *********************************** */
 // When size is submitted by the user, call makeGrid()
-$("#submitSize").on("click", makeGrid);
+$('#submitSize').on('click', makeGrid);
 
-// When a swatch color is clicked, delegate the swatch colour to the color picker
-$(".quickPick-panel").on("click", "div.swatch", quickPick);
+// When anywhere in parent quickPick-panel is clicked, delegate the corresponding child swatch div's colour to
+//the colour picker, using jQuery event delegation syntax
+$('.quickPick-panel').on('click', 'div.swatch', quickPick);
 
-// When user wants the UI panel shifted left or right
-$("#shiftPanel").on("click", shiftPanel);
+// When user wants the UI panel shifted left or right, depending on
+//their right-left handedness preference (if using Wacom tablet for eg)
+$('#shiftPanel').on('click', shiftPanel);
 
 
 
